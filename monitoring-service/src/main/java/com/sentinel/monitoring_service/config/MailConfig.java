5071af16cb7539f0d83bad374545ef1887a92ca1
chhotu kumar <chhotusimaria@gemail.com>
2026-03-03 13:43:09 +0530

    Fix MailConfig to use MailProperties instead of @Value

--- monitoring-service/src/main/java/com/sentinel/monitoring_service/config/MailConfig.java
+++ monitoring-service/src/main/java/com/sentinel/monitoring_service/config/MailConfig.java
@@ -1,9 +1,8 @@
 package com.sentinel.monitoring_service.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -12,26 +11,14 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
-    @Value("${spring.mail.host}")
-    private String host;
-
-    @Value("${spring.mail.port}")
-    private int port;
-
-    @Value("${spring.mail.username}")
-    private String username;
-
-    @Value("${spring.mail.password}")
-    private String password;
-
     @Bean
-    public JavaMailSender javaMailSender() {
+    public JavaMailSender javaMailSender(MailProperties mailProperties) {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
+        mailSender.setHost(mailProperties.getHost());
+        mailSender.setPort(mailProperties.getPort());
+        mailSender.setUsername(mailProperties.getUsername());
+        mailSender.setPassword(mailProperties.getPassword());
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
